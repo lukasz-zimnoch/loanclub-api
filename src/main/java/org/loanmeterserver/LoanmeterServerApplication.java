4b673bf2dc946680ca77c9be270ea820a59a97a3
Łukasz Zimnoch <lukaszzimnoch1994@gmail.com>
2019-05-28 17:33:26 +0200

    Initial commit

---
+++ src/main/java/org/loanmeterserver/LoanmeterServerApplication.java
@@ -0,0 +1,13 @@
+package org.loanmeterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanmeterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanmeterServerApplication.class, args);
+	}
+
+}
